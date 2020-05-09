eb0d0f15562900734305139e66ad70fd25322a89
mila mamat <mila.windsor@gmail.com>
2020-05-08 22:42:50 -0400

    changed the design of the intro about and skills page

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-console.log()
- $(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > (window.innerHeight*0.1));
-    });
+       $(window).scroll(function () {
+           if ($(document).scrollTop() > 50) {
+               $('.navbar').addClass('affix');
+               console.log("OK");
+           } else {
+               $('.navbar').removeClass('affix');
+           }
+       });
 
+       $('.navTrigger').click(function () {
+           $(this).toggleClass('active');
+           console.log("Clicked menu");
+           $("#mainListDiv").toggleClass("show_list");
+           $("#mainListDiv").fadeIn();
+       });
 
-// window.onbeforeunload = function () {
+//        window.onbeforeunload = function () {
 //    window.scrollTo(0, 0);
 //  }
-
